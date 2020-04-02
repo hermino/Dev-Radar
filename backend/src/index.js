e714bd997e60cf9d20bc887e81e79694f219f474
hermino <herminojunior@gmail.com>
2020-04-02 15:52:16 -0400

    Criando o frontend da aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require("cors");
 const app = express();
 
 mongoose.connect("mongodb+srv://hermino:gDkNwlpyglUyStBy@cluster0-qzcqd.mongodb.net/devradar?retryWrites=true&w=majority", {
@@ -9,6 +10,7 @@ mongoose.connect("mongodb+srv://hermino:gDkNwlpyglUyStBy@cluster0-qzcqd.mongodb.
     useCreateIndex: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.listen(3333);
